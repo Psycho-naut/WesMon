812b643edbff4c0d428735f9586cf589ff16a4da
Psycho-naut <cico@MacBook-Pro-di-Cico.local>
2021-06-02 01:34:44 +0200

    02/06/2021

--- WesMon.sh
+++ WesMon.sh
@@ -1,47 +1,117 @@
 #!/bin/bash
 
 # VIEW ALL USER
-.<<FUNZ
+#.<<FUNZ
+
+ICON(){
+
+    echo """
+     ____  __.__                           _____                       
+    |    |/ _|__| ______ ______           /     \ _____ _______ ___.__.
+    |      < |  |/  ___//  ___/  ______  /  \ /  \\\\__  \\\\_  __ <   |  |
+    |    |  \|  |\___ \ \___ \  /_____/ /    Y    \/ __ \|  | \/\___  |
+    |____|__ \__/____  >____  >         \____|__  (____  /__|   / ____|
+            \/       \/     \/                  \/     \/       \/     
+    Type 'help' for list command
+    Type 'quit' for exit
+            """
+}
 
 HELP(){
     
     echo """
-RUN_KISMET          :   run kismet 
+____________________ ______________________________________________________
 
-CREATE_NEW_USER     :   create -n -u <NAME_NEW_USER> -p <PASSWORD_NEW_USER>
-CREATE_NEW_DB       :   create -n -db <NAME_NEW_DATABASE>
-SHOW_USERS          :   show -us
-SHOW_DB             :   show -db
+CREATE_NEW_DB       :   create db 
+SHOW_DB             :   show db
+CREATE TABLE        :   create table
+MAKE BACKUP         :   
+
+CONVERT OUTPUT
+___________________ _______________________________________________________
+FROM KISMET IN KML : convert kml
+FROM KISMET IN CSV : convert csv
+
     """
 }
 
-ROOT_USER(){
-    echo "Root Password:\t"
-    read -s PASSW_ROOT
-}
 
 CONVERT_OUTPUT_ON_KML(){
     pwd
-    PATH_CONVERT=$(grep "DEFAULT_CONVERT" WesMon.conf | cut -d "=" -f2-)
-    cd $PATH_CONVERT
+#    PATH_CONVERT=$(grep "DEFAULT_CONVERT" WesMon.conf | cut -d "=" -f2-)
     pwd
 
+    DATA=""
+    FILE=$(ls | grep $DATA)
+    GPS=$(ls | grep "$DATA.gpsxml")
+    NTX=$(ls | grep "$DATA.netxml")
+    KML=$(ls | grep "$DATA.kml")
+    
+
+    echo -e "File captured"
+    ls -hl
+    echo -e "insert date:\t"
+    read DATA
+    sudo python netxml2kml.py --kml -o Output$DATA $GPS $NTX
+    mv $GPS $NTX DATA
+    mv $KML KML_FILE
 }
 
-START_KISMET(){
-    pwd
-    PATH_START=$(grep "DEFAULT_START" WesMon.conf | cut -d "=" -f2-)
-    cd $PATH_START
-    pwd
-    #sudo kismet
+
+CONVERT_FILE_CSV(){
+    echo -e "File captured"
+    ls -hl
+    echo -e "insert date:\t"
+    read DATA
+    echo "Pulizia file ..."
+    KML=$(ls | grep "Output$DATA.kml")
+    cat $KML | sed -n '/SSID:/,/]]/p' >> Output0.tmp
+    cat Output0.tmp | sed 's/<[^>]*>//g' >> Output01.tmp
+    tr -d "]>" < Output01.tmp > Output02.tmp
+    echo "Conversione file ..."
+
+    # CONVERT FILE TXT IN CSV
+    awk 'BEGIN {i=1} NF { if (i<=7) { printf("%s,", $0); i++ } else { print; i = 1} }' Output02.tmp >> Output$DATA.csv
+    echo "File convertito"
 }
+
+CREATE_CSV_TABLE(){
+    echo "Insert name DB"
+    read DATABASE
+    echo "Insert name TABLE"
+    read NAME_TABLE
+    mysql -u root -e"
+    use $DATABASE;
+    CREATE TABLE $NAME_TABLE
+    (SSID VARCHAR(255) NULL,
+    MAC VARCHAR(255) NULL,
+    Manuf VARCHAR(255) NULL,
+    Type VARCHAR(255) NULL,
+    CHANNEL VARCHAR(255) NULL,
+    Encryption VARCHAR(255) NULL,
+    LASTTIME VARCHAR(255) NULL,
+    GPS VARCHAR(255) NULL)"
+}
+
+LOAD_CSV(){
+    echo "Insert name Table"
+    reat NAME_TB
+    echo "Insert file csv"
+    ls -l *.csv* /opt/Kismet/KML_FILE
+    reat DATI
+    mysql -u root -e"
+    LOAD DATA 
+    INFILE $DATI
+    INTO TABLE $NAME_TB
+    FIELDS TERMINATED BY ','"
+}
+
 
 CREATE_USER(){
     #ROOT_USER
-
-    echo "Name of user:\t"
+    echo -e "Name of user:\t"
     read NEW_USER
-    echo "Select new password:\t"
+    echo -e "Select new password:\t"
     read -s NEW_PASSW
 
     mysql -uroot -e "CREATE USER '$NEW_USER'@'localhost' IDENTIFIED BY '$NEW_PASSW'"
@@ -54,41 +124,59 @@ SHOW_USERS(){
 }
 
 CREATE_DB(){
-    echo "Name new DB:\t"
-    read NAME_DB
+NON LEGGE LA VARIABILE
 
-    mysql -uroot -p $PASSW -e "CREATE DATABASE $NAME_DB;"
-}
+    #echo "Name new DB:\t"
+    DATABASE=""
+    echo -e "Insert name of new Database:\t"
+    mysqladmin create $DATABASE}
 
 SHOW_DB(){
-    mysql -uroot -p $PASSR -e "SHOW DATABASES"
+    mysql -uroot -e "SHOW DATABASES"
 }
-#FUNZ
+
+SHOW_TABLE(){
+    SHOW_DB
+    echo "Select Db"
+    read SELECT_DB
+    mysql -u root -e"
+    use $SELECT_DB;
+    SHOW TABLES"
+}
 
 
 main(){
 
-    echo "PROGRAMMAAAA"
     read COMMAND
 
     for command in case
     do
         case $COMMAND in
-            "show -u" )
+            "show u" )
                 SHOW_USERS ;;
-            "show -db" )
+            "show db" )
                 SHOW_DB ;;
             "help" )
                 HELP ;;
-            "run kismet" )
-                START_KISMET ;;
-
+            "create user" )
+                CREATE_USER  ;;
+            "create db" )
+                CREATE_DB  ;;
+            "convert kml" )
+                CONVERT_OUTPUT_ON_KML ;;
+            "convert csv" )
+                CONVERT_FILE_CSV ;;
+            "show table" )
+                SHOW_TABLE ;;
 
         esac
     main
     done
 }
-FUNZ
+clear
+ICON
+main
+#FUNZ
 
 .<<testok
 echo "Root Password:\t"
@@ -119,5 +207,13 @@ read NAME_DB
 sudo mysql --user="$USR" --password="$PASSR" --execute="CREATE DATABASE $NAME_DB;"
 
 sudo mysql --user="$USR" --password="$PASSR" --execute="SHOW DATABASES"
+
+# Import Csv in DB
+
+echo "Select file:"
+read $FILE 
+
+sudo mysql -uroot -e "LOAD DATA INFILE $FILE INTO TABLE"
+
 
 testok
--- install.sh
+++ install.sh
@@ -8,14 +8,19 @@ clear
 echo -e '-Library Gps \e[32mINSTALLED'
 echo
 
+# Install xmllint
+echo -e '\e[31m** \e[0mInstall XmllInt\e[31m**\e[0m'
+sudo apt-get install libxml2-utils 
+echo -e '\e[32mINSTALLED'
+
 #Install Kismet-2016
 echo -e '\e[31m**\e[0mDownload an install Kismet\e[31m**\e[0m'
 echo
 cd /opt
-mkdir Kismet
+sudo mkdir Kismet
 cd /opt/Kismet
-mkdir DATA
-.<<Old_Installer
+sudo mkdir DATA
+#.<<Old_Installer
 wget http://www.kismetwireless.net/code/kismet-2016-07-R1.tar.xz
 tar -xvf kismet-2016-07-R1.tar.xz
 cd kismet-2016-07-R1
@@ -24,8 +29,8 @@ sudo make dep
 sudo make
 sudo make install
 echo
-Old_Installer
-sudo apt-get install kismet
+#Old_Installer
+#sudo apt-get install kismet
 echo -e '\e[31m**\e[0mKismet Installed\e[31m**\e[0m'
 sleep 3
 clear
@@ -39,7 +44,7 @@ echo -e '\e[31m**\e[0mInstall NetXml2Kml\e[31m**\e[0m'
 echo
 cd /opt/Kismet/DATA
 wget https://gist.githubusercontent.com/ScottHelme/5c6869e17c3e9c8b2034dc8fc13e180b/raw/31c2d34f66748b6bd26415fd7d120c06b3d92eaf/netxml2kml.py -O netxml2kml.py
-mkdir OLD; mkdir KML_FILE;
+sudo mkdir OLD;sudo  mkdir KML_FILE;
 echo -e '\e[31m**\e[0mNetXml2Kml installed\e[31m**\e[0m '
 sleep 3
 clear
@@ -50,7 +55,7 @@ echo -e '-NetXml2Kml   \e[32mINSTALLAED'
 # Install MariaDB on Raspberry
 echo -e '\e[31m**\e[0mInstall MariaDB\e[31m**\e[0m'
 clear
-sudo apt install mariadb-server
+sudo apt-get install mariadb-server
 echo "You want make Secure Installation? (Y/n)"
 read CHOIS
 
